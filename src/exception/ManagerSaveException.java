ecce6aa09bb56b824c53b51e5c498d7d3d504d1c
anton-tt <carduelis@yandex.ru>
2023-03-30 13:19:33 +0700

    ТЗ6, перед первым ревью

--- src/exception/ManagerSaveException.java
+++ src/exception/ManagerSaveException.java
@@ -1,24 +1,8 @@
 package exception;
 
-import java.io.IOException;
-
 public class ManagerSaveException extends RuntimeException {
     public ManagerSaveException(String cause) {
         super(cause);
     }
 
-
-
 }
-/*public class ManagerSaveException extends RuntimeException {
-    public ManagerSaveException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public ManagerSaveException(String u, void l) {
-    }
-
-    /*public String getDetailMessage() {
-        return "Ошибка ввода: " + getMessage();
-    }
-}*/
